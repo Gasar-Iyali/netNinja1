1f3b35ce1a71fef0e04c7d38b9aa44e893bc59de
Zack Amata <gasariyali09@gmail.com>
2021-04-21 10:31:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(!$conn){
 }
 
 // write the query to get all the data
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make the query and get the result
 $result = mysqli_query($conn, $sql);
@@ -23,13 +23,38 @@ mysqli_free_result($result);
 // close the connection
 mysqli_close($conn);
 
-print_r($pizzas);
+// print_r($pizzas);
+
  ?>
 
   <!DOCTYPE html>
   <html lang="en">
 
     <?php include('templates/header.php'); ?>
+
+    <h4 class="center grey-text">Awesome Pizzas</h4>
+
+    <div class="container">
+      <div class="row">
+
+          <?php foreach ($pizzas as $pizza){ ?>
+
+            <div class="col s6 md3">
+              <div class="card z-depth-0">
+                <div class="card-content center">
+                  <h6> <?php echo htmlspecialchars($pizza['title']); ?> </h6>
+                  <div class=""><?php echo htmlspecialchars($pizza['ingredients']); ?>
+                  </div>
+                  <div class="card-action right-align">
+                    <a href="#" class="brand-text">more info</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+      </div>
+
+    </div>
 
     <?php include('templates/footer.php'); ?>
 
